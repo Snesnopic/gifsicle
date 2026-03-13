f33f6bb909288aff6ceebf50cd54ab91b375bd5f
Snesnopic <snesnopic@gmail.com>
2026-03-13 19:41:33 +0100

    Solve compilation problems for Windows and Linux

--- src/gifsicle.h
+++ src/gifsicle.h
@@ -9,6 +9,9 @@
 
 #ifdef __cplusplus
 #define G_THREAD_LOCAL thread_local
+#elif defined(_MSC_VER)
+// msvc-specific thread local storage
+#define G_THREAD_LOCAL __declspec(thread)
 #else
 #define G_THREAD_LOCAL _Thread_local
 #endif
